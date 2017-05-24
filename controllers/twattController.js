5d960e4a540e86da8b86e013c28fa7a2eff4de7a
arahito <uarahito@yahoo.com>
2017-05-24 12:55:58 +0700

    update fitur recent Twatt

--- controllers/twattController.js
+++ controllers/twattController.js
@@ -5,11 +5,25 @@ require('dotenv').config()
 methods.recentTwatt = (req, res) => {
   twatt.getOauth(oauth => {
     oauth.get(
-      'https://api.twitter.com/1.1/statuses/user_timeline.json?%40jakarta=twitterapi&count=2',
+      `https://api.twitter.com/1.1/statuses/user_timeline.json?${encodeURIComponent(req.body.akun)}=twitterapi&count=${encodeURIComponent(req.body.count)}`,
       process.env.TOKEN, //test user token
       process.env.TOKEN_SECRET, //test user secret
       function (e, data){
         if (e) console.error(e);
+        let dataParse = JSON.parse(data)
+        console.log(dataParse);
+        let tempData = []
+        dataParse.forEach(record => {
+          let newData = {
+            created_at: record.created_at,
+            text: record.text,
+            user: record.user.name,
+            retweet_count: record.retweet_count,
+            language: record.lang
+          }
+          tempData.push(newData)
+        })
+        res.send(tempData)
         res.send(data)
       });
   })
